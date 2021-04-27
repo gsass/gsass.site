e4fb34ea51f22648a466d2c3b9fac4eadd7ca248
George Sass <gsass@graybox.localdomain>
2021-04-27 14:14:12 -0400

    Stub out content files

---
+++ src/routes.js
@@ -0,0 +1,22 @@
+/*
+* Routes for this app are all hash-based and all here
+* All routes must have attributes `name` and `hash`
+* Routes may also have a `label` which allows them to appear in the nav,
+* and a `contentPath` to load associated markdown content for the page
+*/
+const routes = [
+  { name: 'home',
+    hash: '#',
+    label: '👋 Hi!',
+    contentPath: 'home.md'},
+  { name: 'resume',
+    hash: '#resume',
+    label: 'Résumé',
+    contentPath: 'resume.md'},
+  { name: 'portfolio',
+    hash: '#portfolio',
+    label: 'Stuff I\'ve Done',
+    contentPath: 'portfolio.md'}
+];
+
+export default routes;
